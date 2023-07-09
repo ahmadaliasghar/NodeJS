59c46886568aff49ed715b4b464324b37a0e0ab4
ahmadaliasghar <ahmadali2882@yahoo.com>
2023-07-09 22:04:12 +0500

    Chapter 5 Express

---
+++ chapter5/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const express = require('express')
+const app = express()
+const path = require('path');
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter { };
+
+const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+const PORT = process.env.PORT || 3500;
+
+
+app.get('^/$|/index(.html)?', (req, res)=> {
+    // res.send("Hello World")
+    // res.sendFile('./views/index.html', {root: __dirname})
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+app.get('/new-page(.html)?', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+})
+app.get('/old-page(.html)?', (req, res)=> {
+    // res.redirect('/new-page.html') //302 by default
+    res.redirect(301,'/new-page.html') //301
+})
+app.get('/*', (req, res)=> {
+    // res.sendFile(path.join(__dirname, 'views', '404.html')) //404
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')) //404
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
